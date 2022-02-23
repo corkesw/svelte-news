71e3aa0341ad4836e2ec3b06f48a1d7b9adaf4fb
corkesw <home@stephencorke.com>
2022-02-23 16:38:48 +0000

    add inital api call in component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script>
-	export let name;
+  import Articles from "./components/Articles.svelte";
+  let topic = "coding";
+  let sortBy = "title";
+  let order = "asc";
+  let page = "1";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Articles bind:topic bind:sortBy bind:order bind:page />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ src/components/Articles.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { getArticles } from "../utils/api";
+
+  export let topic = "banana"
+  export let sortBy = "title";
+  export let order = "asc";
+  export let page = "1";
+
+  console.log(topic);
+
+  onMount(
+    getArticles(topic, sortBy, order, page).then((res) => {
+      console.log(res);
+      console.log("component did mount");
+    })
+  );
+</script>
+
+<div>
+  <p>Articles</p>
+</div>
