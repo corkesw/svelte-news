7a6ea9d0b43ada209bd3155cf333fb1b300de345
corkesw <home@stephencorke.com>
2022-02-24 14:02:49 +0000

    add login component

---
+++ src/components/Login.svelte
@@ -0,0 +1,30 @@
+<script>
+
+
+</script>
+
+
+<div className="login">
+    <form className="loginform" onSubmit={handleLogin}>
+      <label htmlFor="username" name="username"></label>
+      <input
+        onChange={(e) => {
+          setErr(null);
+          setUserInput(e.target.value);
+        }}
+        value={userInput}
+        type="text"
+        name="username"
+        placeholder="username"
+      >
+      <button>Login</button>
+      <br />
+      {#if err}
+      <p class="errormessage">{err}</p>
+      {/if}
+      {#if user}
+      <p>You are logged in as {user}</p>
+      {/if}
+    </form>
+    <span className="spacer"></span>
+  </div>
